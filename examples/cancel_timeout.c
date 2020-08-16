56a09826cec6ed5c828747ff8e5639f6dfcbb604
shädam <47268949+supahero1@users.noreply.github.com>
2020-08-16 10:48:38 +0200

    Update cancel_timeout.c

--- examples/cancel_timeout.c
+++ examples/cancel_timeout.c
@@ -20,6 +20,7 @@ int main() {
  
   printf("number is %d\n", *number);
   unsigned short int timeoutID = SetCancelTimeout(editNumber, 500, number); // automatically resizes (if needed) memory to keep the timeouts, use SetMultipleCancelTimeouts in addition to ResizeTimeoutStates if you want
+  // the first call to set up a cancelable timeout does not allocate any more memory. When initialising the additional memory with USE_CTIMEOUT, it already allocates enough for 1 timeout.
   (void) TimeoutedCancelTimeout(timeoutID, 100); // casted to void to get rid of the returned timeoutID, if you want to instantly cancel a timeout, use CancelTimeout
   SetNoCancelTimeout(say, 600, number); // printing the number some time after
   // unsigned short int is used for timeoutIDs, because I don't expect anyone wanting to create more than 65535 threads at the same time. Anyway, if you would like to do so, just change a few types in the timeout.h file.
