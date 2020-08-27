83fb974ef32aa892853b049b604da1d8d1c5caa3
shädam <47268949+supahero1@users.noreply.github.com>
2020-08-27 23:32:07 +0200

    Fix dumb issues

--- examples/cancel_timeout.c
+++ examples/cancel_timeout.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define _USE_CTIMEOUT // letting the file know we want to allocate additional memory for cancelable timeouts
-// needed memory: sizeof(char) + sizeof(char*) + sizeof(short int) * 2 + sizeof(short int*) + nOfCancelableTimeouts * (sizeof(char) + sizeof(short int))
-// additional memory is allocated for the lifetime of a timeout for various things enabling canceling the timeout (the same case is with non-cancelable ones, but they take significantly less memory. It is still almost nothing though)
+#define _USE_CTIMEOUT // letting the file know we want to allocate additional memory for cancelable timeouts and to enable more functions
 #include "timeout.h"
 
 void editNumber(int* num) {
@@ -19,14 +16,14 @@ int main() {
   *number = 5;
  
   printf("number is %d\n", *number);
-  unsigned short int timeoutID = SetCancelTimeout(editNumber, 500, number); // automatically resizes (if needed) memory to keep the timeouts, use SetMultipleCancelTimeouts in addition to ResizeTimeoutStates if you want
+  unsigned int timeoutID = SetCancelTimeout(editNumber, 500, number); // automatically resizes (if needed) memory to keep the timeouts, use SetMultipleCancelTimeouts in addition to ResizeTimeoutStates if you want
   // the first call to set up a cancelable timeout does not allocate any more memory. When initialising the additional memory with USE_CTIMEOUT, it already allocates enough for 1 timeout.
   (void) TimeoutedCancelTimeout(timeoutID, 100); // casted to void to get rid of the returned timeoutID, if you want to instantly cancel a timeout, use CancelTimeout
   SetNoCancelTimeout(say, 600, number); // printing the number some time after
   // unsigned short int is used for timeoutIDs, because I don't expect anyone wanting to create more than 65535 threads at the same time. Anyway, if you would like to do so, just change a few types in the timeout.h file.
  
-  puts("press any key to exit");
-  getch();
+  //puts("press any key to exit");
+  getc(stdin);
   free(number);
   return 0;
 }
