09596576ff5a677d61d2f641be8907c800c4399e
shädam <47268949+supahero1@users.noreply.github.com>
2020-09-06 18:00:55 +0200

    Update no_cancel_timeout.c

--- examples/no_cancel_timeout.c
+++ examples/no_cancel_timeout.c
@@ -13,8 +13,8 @@ int main() {
   int number = 5;
   printf("number is %i\n", number);
   (void) SetTimeout(editNumber, 500, &number); // we don't need to care about the return value if we will not cancel the timeout
-  (void) SetTimeout(say, 600, &number);
-  
+  (void) SetTimeout(say, 600, &number); // time is measured in milliseconds, even though nanosleep() is used
+  // expected output: 5 1, which means success in changing the number after some time
   getc(stdin);
   return 0;
 }
